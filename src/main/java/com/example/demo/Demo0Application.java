7342faa31a136a546f7d193b7650ae5ee7c93583
Jonathan Pyrak <bobcatpyrak@gmail.com>
2020-10-15 15:01:30 -0400

    initial commit .1

--- src/main/java/com/example/demo/Demo0Application.java
+++ src/main/java/com/example/demo/Demo0Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo0Application {
+public class Demo0Application 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(Demo0Application.class, args);
 	}
 
